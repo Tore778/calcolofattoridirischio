f03cac078ee784290073d67f4e90a89e6d93b619
Salvatore Notaro <salvatorenotaro22@gmail.com>
2025-09-22 02:19:09 +0200

    feat(home): tailwind home with header/hero/footer and include loader

---
+++ docs/home/js/ui.home.js
@@ -0,0 +1,8 @@
+﻿import { loadIncludes } from "./dom.js";
+(async ()=>{
+  await loadIncludes();
+  const btn = document.querySelector("[data-role='open-verify']");
+  if(btn) btn.addEventListener("click", e=>{
+    e.preventDefault(); location.href = "../verify/index.html";
+  });
+})();
---
+++ www/home/js/dom.js
@@ -0,0 +1,8 @@
+﻿export async function loadIncludes(){
+  const nodes = document.querySelectorAll("[data-include]");
+  await Promise.all([...nodes].map(async el=>{
+    const url = el.getAttribute("data-include");
+    const html = await fetch(url).then(r=>r.text());
+    el.outerHTML = html;
+  }));
+}
